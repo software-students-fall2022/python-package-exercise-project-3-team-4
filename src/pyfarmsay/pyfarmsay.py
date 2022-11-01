ff77ec3288622e433ab4c1d9cd7ca48f141865f4
sarah-altowaity1 <sa5793@nyu.edu>
2022-11-01 18:37:35 -0400

    added basic .toml file

--- src/pyfarmsay/pyfarmsay.py
+++ src/pyfarmsay/pyfarmsay.py
@@ -1,7 +1,7 @@
 from textwrap import wrap
 
 from util import animalsay, INDENT_DEFAULT
-import animals
+import src.pyfarmsay.animals as animals
 
 def cowsay(message, maxwidth=100, indent=INDENT_DEFAULT):
     return animalsay(wrap(message, maxwidth), animal=animals.COW, indent=indent)

6d215dad5b8d99d290df85aae03f6ed6148b83d2
hpdouglass <hpdouglass10@gmail.com>
2022-11-02 12:03:20 -0400

    feature: Add chicken, sheep, penguin, dog

--- pyfarmsay.py
+++ pyfarmsay.py
@@ -1,10 +1,22 @@
 from textwrap import wrap
 
-from util import animalsay, INDENT_DEFAULT
+from util import animalsay, INDENT_DEFAULT, MAXWIDTH_DEFAULT
 import animals
 
-def cowsay(message, maxwidth=100, indent=INDENT_DEFAULT):
+def cowsay(message, maxwidth=MAXWIDTH_DEFAULT, indent=INDENT_DEFAULT):
     return animalsay(wrap(message, maxwidth), animal=animals.COW, indent=indent)
 
-def pigsay(message, maxwidth=100, indent=INDENT_DEFAULT):
+def pigsay(message, maxwidth=MAXWIDTH_DEFAULT, indent=INDENT_DEFAULT):
     return animalsay(wrap(message, maxwidth), animal=animals.PIG, indent=indent)
+
+def chickensay(message, maxwidth=MAXWIDTH_DEFAULT, indent=INDENT_DEFAULT):
+    return animalsay(wrap(message, maxwidth), animal=animals.CHICKEN, indent=indent)
+
+def sheepsay(message, maxwidth=MAXWIDTH_DEFAULT, indent=INDENT_DEFAULT):
+    return animalsay(wrap(message, maxwidth), animal=animals.SHEEP, indent=indent)
+
+def penguinsay(message, maxwidth=MAXWIDTH_DEFAULT, indent=INDENT_DEFAULT):
+    return animalsay(wrap(message, maxwidth), animal=animals.PENGUIN, indent=indent)
+
+def dogsay(message, maxwidth=MAXWIDTH_DEFAULT, indent=INDENT_DEFAULT):
+    return animalsay(wrap(message, maxwidth), animal=animals.DOG, indent=indent)
